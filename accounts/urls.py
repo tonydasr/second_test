15d54848613357b90d3ba1c90e6294a0e0eb7d09
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-05-17 12:23:49 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     re_path(r'^register/$', register_view, name='register'),
     re_path(r'^logout/$', logout_view, name='logout'),
     re_path(r'^select_user/$', select_user, name='select_user'),
+    re_path(r'^change-password/$', change_password_view, name='change_password'),
+
 ]
