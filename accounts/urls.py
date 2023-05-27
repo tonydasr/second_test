0fb587ac88dfedd2d145cb103c9c9bc54102d13b
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-05-27 16:43:14 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,7 @@ from .views import (
     register_view,
     logout_view,
     select_user,
-    change_password_view,
-    show_password_view,
-    decrypt_password_view
+    change_password_view
 )
 
 app_name = 'accounts'
@@ -18,7 +16,6 @@ urlpatterns = [
     re_path(r'^register/$', register_view, name='register'),
     re_path(r'^logout/$', logout_view, name='logout'),
     re_path(r'^select_user/$', select_user, name='select_user'),
-    re_path(r'^change-password/$', change_password_view, name='change_password'),
-    re_path(r'^show-password/$', show_password_view, name='show_password'),
-    re_path(r'^decrypt-password/$', decrypt_password_view, name='decrypt_password'),
+    re_path(r'^change-password/$', change_password_view, name='change_password')
+
 ]
