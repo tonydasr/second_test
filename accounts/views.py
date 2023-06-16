4ed3e02c56b42fecebfdf11f68d8e01e09256c0a
tonydasr <136280620+tonydasr@users.noreply.github.com>
2023-06-16 15:55:14 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -175,7 +175,7 @@ def invest_now(request):
                 return render(request, 'accounts/create_starter.html', {'form': form, 'error_message': error_message})
 
             investment.save()
-            
+            return redirect('home')  # Replace 'home' with the appropriate URL name for your home page
     else:
         form = InvestmentForm()
     
